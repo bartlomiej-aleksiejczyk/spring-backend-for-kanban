69c92c1b9fdeda48ca7c4e259169dfd39afd567c
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-06-27 18:56:13 +0200

    Added table with users that is connected with spring boot

--- spring-backend/src/main/java/com/example/springbackend/security/User.java
+++ spring-backend/src/main/java/com/example/springbackend/security/User.java
@@ -1,6 +1,28 @@
 package com.example.springbackend.security;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Anti boilerplate
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+@Table(name = "_user")
 public class User {
-   private Integer id;
-    private String name;
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String username;
+    private String password;
+    private String email;
+    private String avatar;
 }
