26e2127f11907e1d255133a64380b6810961396a
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-06-27 23:52:58 +0200

    Added filter, user and repo for Jwt Spring Security

---
+++ spring-backend/src/main/java/com/example/springbackend/security/JwtRequestAuthFilter.java
@@ -0,0 +1,25 @@
+package com.example.springbackend.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+@RequiredArgsConstructor
+public class JwtRequestAuthFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authenticationHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authenticationHeader == null||!authenticationHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+    }
+}
---
+++ spring-backend/src/main/java/com/example/springbackend/security/Role.java
@@ -0,0 +1,6 @@
+package com.example.springbackend.security;
+
+public enum Role {
+    USER,
+    ADMIN
+}
